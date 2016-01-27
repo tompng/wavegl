83638959184e6befc0d30c41caf58fa923217beb
tompng <tomoyapenguin@gmail.com>
2016-01-28 03:13:11 +0900

    particle system

--- particle.js
+++ particle.js
@@ -11,8 +11,56 @@ Particle.geometry = function(num){
   geometry.addAttribute('normal', new THREE.BufferAttribute(narr, 3));
   return geometry;
 }
+Particle.System = function(scene){
+  var geometries = {};
+  this.prepareGeometry = function(n){
+    var geom = geometries[n];
+    if(!geom)geom = Particle.geometry(n);
+    return geom;
+  }
+  var windowSize = 800;
+  var particles=[];
+  this.fire = function(num, position, time, material){
+    var geom = this.prepareGeometry(num);
+    var mesh = new THREE.PointCloud(geom, material.clone());
+    mesh.renderOrder = 1;
+    mesh.position.x = position.x;
+    mesh.position.y = position.y;
+    mesh.position.z = position.z;
+    mesh.material.uniforms.time.value = 0;
+    mesh.material.uniforms.windowSize.value = windowSize;
+    scene.add(mesh);
+    particles.push({
+      mesh: mesh,
+      time: performance.now()/1000,
+      duration: time
+    });
+  }
+  this.update = function(winsize){
+    windowSize = winsize;
+    var lives = [];
+    var deads = [];
+    var time=performance.now()/1000;
+    particles.forEach(function(p){
+      var t = (time-p.time)/p.duration;
+      if(t>1){
+        deads.push(p);
+        return;
+      }
+      p.mesh.material.uniforms.time.value = t;
+      p.mesh.material.uniforms.windowSize.value = windowSize;
+      lives.push(p);
+    })
+    particles = lives;
+    deads.forEach(function(p){
+      scene.remove(p.mesh);
+      p.mesh.material.dispose();
+    })
+  }
+}
 
-Particle.shaderBase=function(func, uni){
+
+Particle.materialBase=function(func, uni){
   var uniforms = {
     windowSize: {type: 'f', value: 800},
     maxSize: {type: 'f', value: 20},
@@ -64,30 +112,24 @@ Particle.shaderBase=function(func, uni){
 }
 
 
-Particle.vortexShader=function(){
-  return Particle.shaderBase(function(){/*
-    float theta = time*16.0+0.5*position.x*min(4.0*time,1.0);
-    size=6.0*time*(1.0-time)*(1.0-time);
-    pos = 4.0*time*time*(3.0-2.0*time)*vec3(cos(theta),sin(theta),1.0-cos(3.0*theta)*(1.0-time)+time*sin(2.0*theta))+(0.2+time)*normal*0.4;
-    color = vec3(0.5,0.4,0.3);
-  */});
-}
+Particle.vortexMaterial=Particle.materialBase(function(){/*
+  float theta = time*16.0+0.5*position.x*min(4.0*time,1.0);
+  size=6.0*time*(1.0-time)*(1.0-time);
+  pos = 4.0*time*time*(3.0-2.0*time)*vec3(cos(theta),sin(theta),1.0-cos(3.0*theta)*(1.0-time)+time*sin(2.0*theta))+(0.2+time)*normal*0.4;
+  color = vec3(0.5,0.4,0.3);
+*/});
 
-Particle.rippleShader=function(){
-  return Particle.shaderBase(function(){/*
-    float theta = 1000.0*position.x+0.02*sin(4.0*position.y*time);
-    size=2.0;
-    pos = vec3(vec2(cos(theta),sin(theta))*(0.5+4.0*time+0.4*position.z*time+0.4*position.x*(1.0-time)), 0.1+0.05*normal.z);
-    color = time*time*(1.0-time)*vec3(1.0,0.8,0.6);
-  */});
-}
+Particle.rippleMaterial=Particle.materialBase(function(){/*
+  float theta = 1000.0*position.x+0.02*sin(4.0*position.y*time);
+  size=2.0;
+  pos = vec3(vec2(cos(theta),sin(theta))*(0.5+4.0*time+0.4*position.z*time+0.4*position.x*(1.0-time)), 0.1+0.05*normal.z);
+  color = time*time*(1.0-time)*vec3(1.0,0.8,0.6);
+*/});
 
-Particle.divergenceShader=function(){
-  return Particle.shaderBase(function(){/*
-    float phase = (1000.0*position.x+2.0*time);
-    phase = phase - floor(phase);
-    size=2.0;
-    pos = 1.0*normalize(normal)*phase+vec3(0,0,1);
-    color = phase*phase*(1.0-phase)*vec3(1.0,0.8,0.6);
-  */});
-}
+Particle.starMaterial=Particle.materialBase(function(){/*
+  float phase = (1000.0*position.x+2.0*time);
+  phase = phase - floor(phase);
+  size=2.0;
+  pos = 1.0*normalize(normal)*phase+vec3(0,0,1);
+  color = phase*phase*(1.0-phase)*vec3(1.0,0.8,0.6);
+*/});
